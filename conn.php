cacdf022166280dd0b829077e8ffdf4fed409a6e
Lily Alejandra Pardo <82472356+Lilypar59@users.noreply.github.com>
2024-09-18 22:59:56 -0500

    changes

---
+++ conn.php
@@ -0,0 +1,6 @@
+<?php
+$conn = mysqli_connect('localhost', 'root', '', 'exporta_excel');
+//$conn = mysqli_connect('dbwpartesanias.cpuokwowaq40.us-east-1.rds.amazonaws.com', 'admin', 'claveAWS2024', 'exporta_excel');
+if (!$conn) {
+	die("Error: Fallo al conectar con la base de datos");
+}
